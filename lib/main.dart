58e21a9e0542d20a00f40930279897917906ea8a
Hendy Nur Sholeh <hendynursholeh7213@gmail.com>
2025-08-06 15:45:09 +0800

    stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,10 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Hendy Nur Sholeh, world!'),
+          title: Text('Hendy Nur Sholeh, world!', style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold)),
         ),
         body: Center(
-          child: Heading(text: 'TodoList App'),
+          child: BiggerText(text: "Tombol gede"),
         ),
       ),
     );
@@ -37,6 +37,47 @@ class Heading extends StatelessWidget {
         fontSize: 24.0,
         fontWeight: FontWeight.bold,
       ),
+    );
+  }
+}
+
+class BiggerText extends StatefulWidget {
+  final String text;
+
+  const BiggerText({Key? key, required this.text}) : super(key: key);
+
+  @override
+  State<BiggerText> createState() => _BiggerTextState();
+}
+
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(widget.text, style: TextStyle(fontSize: _textSize, fontWeight: FontWeight.bold, color: Colors.blue)),
+        ElevatedButton(
+          child: const Text("Perbesar", style: TextStyle(color: Colors.white)),
+          onPressed: () {
+            setState(() {
+              if (_textSize == 32.0) {
+                _textSize = 16.0;
+                return;
+              }
+              _textSize = 32.0;
+            });
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue,
+            padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+            textStyle: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          )
+        )
+      ],
     );
   }
 }
